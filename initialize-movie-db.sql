f88208a577c2f7b462193754a3b948898264e043
Matt Huberty <matthewhuberty77@gmail.com>
2020-06-28 13:54:59 -0500

    Added auto-gen uuid

--- initialize-movie-db.sql
+++ initialize-movie-db.sql
@@ -1,4 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE seat_reservations (
+  uuid uuid DEFAULT uuid_generate_v4 (),
   location_id varchar(255) NOT NULL,
   theater_id varchar(255) NOT NULL,
   showing_id varchar(255) NOT NULL,
@@ -6,7 +9,7 @@ CREATE TABLE seat_reservations (
   seat_number smallint NOT NULL
 );
 
-INSERT INTO seat_reservations VALUES 
+INSERT INTO seat_reservations (location_id, theater_id, showing_id, seat_row, seat_number) VALUES 
   ('denver-1232', '2', '1232-2-4-12-2020-20:30', 'A', '2'),
   ('denver-1232', '2', '1232-2-4-12-2020-20:30', 'A', '3'),
   ('denver-1232', '2', '1232-2-4-12-2020-20:30', 'A', '4'),
